5691b17c0b8d37c5eea2ee423d7a8677d234470b
Edward Barcenas <edward.barcenas16@gmail.com>
2022-12-10 17:26:30 -0600

    completed assignment Practice SBA for Errors

---
+++ src/org/perscholas/LoopExample.java
@@ -0,0 +1,85 @@
+package org.perscholas;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class LoopExample {
+
+	public static void main(String[] args) {
+		// Question 1 Try-catch
+		// divide();
+
+		// Question 2
+		TypingPractice type = new TypingPractice();
+		boolean loopAgain = true;
+		while (loopAgain) {
+			try {
+				type.typeABC();
+				loopAgain = false;
+			} catch (myException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/* Question 1
+	 * Create a static void method named divide that has no parameters. Let the user
+	 * input two integers and print the value of dividing the two integers. Use a
+	 * try-catch block to catch an ArithmeticException and InputMismatchException.
+	 * If you have an ArithmeticException print “Please do not use zero”. If you
+	 * have an InputMismatch print “Please only use integers. Use a while loop and a
+	 * boolean to make sure if the user inputs an ArithmeticException or
+	 * InputMismatchException the program does not terminate but lets the user try
+	 * again.
+	 */
+
+	public static void divide() {
+		boolean loopAgain = true;
+		while (loopAgain) {
+			try {
+				Scanner sc = new Scanner(System.in);
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				System.out.println(a / b);
+				loopAgain = false;
+			} catch (InputMismatchException ime) {
+				System.out.println("Please only use integers");
+				// continue;
+			} catch (ArithmeticException e) {
+				System.out.println("Please do not use zero");
+				// continue;
+			}
+		}
+	}
+
+}
+
+/* Question 2
+ * Create a class called MyException that extends Exception. Create a
+ * constructor but from the super class that will let you print out a message.
+ * Create a class called TypingPractice. Create a method called typeABC. Let the
+ * user type a String and if the String does not match “abc” throw new
+ * MyException() with a String as a parameter that says “incorrect input”. Let
+ * the user try again until they type “abc”. Don’t let the program terminate
+ * after the exception is called.
+ */
+
+class myException extends Exception {
+	public myException(String message) {
+		super(message);
+	}
+}
+
+class TypingPractice {
+
+	public void typeABC() throws myException {
+		String string = "";
+		Scanner sc = new Scanner(System.in);
+		string = sc.nextLine();
+		if (!(string.equals("abc"))) {
+			throw new myException("incorrect input");
+		}
+		sc.close();
+	}
+}
